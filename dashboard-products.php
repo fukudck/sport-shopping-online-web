ba0baed3d44a7005d9586e53700376f7d2821c67
NgKhao <bsbnk141@gmail.com>
2024-11-20 14:16:52 +0700

    channge div to span

--- dashboard-products.php
+++ dashboard-products.php
@@ -155,9 +155,9 @@ if ($result->num_rows > 0) {
             if (!empty($products)): ?>
               <?php foreach ($products as $row): ?>
                 <div class="d-block d-sm-flex align-items-center py-4 border-bottom">
-                  <div class="d-block mb-3 mb-sm-0 me-sm-4 ms-sm-0 mx-auto" style="width: 12.5rem;">
+                  <span class="d-block mb-3 mb-sm-0 me-sm-4 ms-sm-0 mx-auto" style="width: 12.5rem;">
                     <img class="rounded-3" src="<?= $row['image_url'] ?>" alt="Product">
-                  </div>
+                  </span>
                   <div class="text-center text-sm-start">
                     <h3 class="h6 product-title mb-2">
                       <?= htmlspecialchars($row['name']) ?>
